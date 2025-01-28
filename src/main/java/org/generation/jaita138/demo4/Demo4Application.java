02fb7c5a6093e94a0fcabb4c5bac5d436fd07d6a
Dominic <118680299+bjdome@users.noreply.github.com>
2025-01-28 15:19:50 +0100

    fase 1

--- src/main/java/org/generation/jaita138/demo4/Demo4Application.java
+++ src/main/java/org/generation/jaita138/demo4/Demo4Application.java
@@ -1,13 +1,45 @@
 package org.generation.jaita138.demo4;
 
+import java.util.List;
+
+import org.generation.jaita138.demo4.db.entity.Utente;
+import org.generation.jaita138.demo4.db.service.UtenteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo4Application {
+public class Demo4Application implements CommandLineRunner{
 
+	@Autowired
+	private UtenteService utenteService;
 	public static void main(String[] args) {
 		SpringApplication.run(Demo4Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		test();
+	}
+
+	public void test() {
+
+		System.out.println("--------------------------------");
+		Utente u1 = new Utente();
+		u1.setNome("Mario");
+		u1.setCognome("Rossi");
+		u1.setUsername("mRossi");
+		u1.setPassword("123456");
+		u1.setCredito(1090);
+		System.out.println("--------------------------------");
+		utenteService.save(u1);
+		System.out.println("--------------------------------");
+		System.out.println(u1);
+		List<Utente> utenti = utenteService.findAll();
+		System.out.println(utenti);
+		System.out.println("--------------------------------");
+		System.out.println("The end");
 	}
 
 }
---
+++ src/main/java/org/generation/jaita138/demo4/db/service/UtenteService.java
@@ -0,0 +1,31 @@
+package org.generation.jaita138.demo4.db.service;
+
+import java.util.List;
+
+import org.generation.jaita138.demo4.db.entity.Utente;
+import org.generation.jaita138.demo4.db.repository.UtenteRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UtenteService {
+
+    @Autowired
+    private UtenteRepo utenteRepo;
+
+    public void save(Utente utente) {
+        utenteRepo.save(utente);
+    }
+
+    public void delete(Long id) {
+        utenteRepo.deleteById(id);
+    }
+
+    public List<Utente> findAll() {
+        return utenteRepo.findAll();
+    }
+
+    public Utente findById(Long id) {
+        return utenteRepo.findById(id).orElse(null);
+    }
+}
