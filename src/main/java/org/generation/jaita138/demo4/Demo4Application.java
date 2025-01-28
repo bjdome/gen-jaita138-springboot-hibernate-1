7bb0e4548c3a5b2da4a3cea4b642d6fc188866c7
Dominic <118680299+bjdome@users.noreply.github.com>
2025-01-28 16:41:25 +0100

    Fase 3

--- src/main/java/org/generation/jaita138/demo4/Demo4Application.java
+++ src/main/java/org/generation/jaita138/demo4/Demo4Application.java
@@ -2,8 +2,8 @@ package org.generation.jaita138.demo4;
 
 import java.util.List;
 
-import org.generation.jaita138.demo4.cli.CliManager;
 import org.generation.jaita138.demo4.db.entity.Utente;
+import org.generation.jaita138.demo4.db.repository.UtenteRepo;
 import org.generation.jaita138.demo4.db.service.UtenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,37 +11,65 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo4Application implements CommandLineRunner{
+public class Demo4Application implements CommandLineRunner {
 
-	@Autowired
-	private UtenteService utenteService;
-	public static void main(String[] args) {
-		SpringApplication.run(Demo4Application.class, args);
-	}
+    @Autowired
+    private UtenteService utenteService;
+    @Autowired
+    private UtenteRepo utenteRepo;
 
-	@Override
-	public void run(String... args) throws Exception {
-		//test();
-		new CliManager(utenteService);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Demo4Application.class, args);
+    }
 
-	public void test() {
+    @Override
+    public void run(String... args) throws Exception {
+        //test();
+        //new CliManager(utenteService);
+        testQuery();
+    }
 
-		System.out.println("--------------------------------");
-		Utente u1 = new Utente();
-		u1.setNome("Mario");
-		u1.setCognome("Rossi");
-		u1.setUsername("mRossi");
-		u1.setPassword("123456");
-		u1.setCredito(1090);
-		utenteService.save(u1);
-		List<Utente> utenti = utenteService.findAll();
-		utenteService.findById(1L);
-		utenteService.delete(utenti.get(0));
-		System.out.println(u1);
-		System.out.println(utenti);
-		System.out.println("--------------------------------");
-		System.out.println("The end");
-	}
+    public void test() {
 
+        System.out.println("--------------------------------");
+        Utente u1 = new Utente();
+        u1.setNome("Mario");
+        u1.setCognome("Rossi");
+        u1.setUsername("mRossi");
+        u1.setPassword("123456");
+        u1.setCredito(1090);
+        utenteService.save(u1);
+        List<Utente> utenti = utenteService.findAll();
+        utenteService.findById(1L);
+        utenteService.delete(utenti.get(0));
+        System.out.println(u1);
+        System.out.println(utenti);
+        System.out.println("--------------------------------");
+        System.out.println("The end");
+    }
+
+    public void testQuery() {
+        System.out.println("--------------------------------");
+        List<Utente> utenti = utenteRepo.findByNomeStartsWith("a");
+        System.out.println(utenti);
+
+        System.out.println("--------------------------------");
+
+        utenti = utenteRepo.findByCreditoGreaterThan(10 * 100);
+        System.out.println(utenti);
+
+        System.out.println("--------------------------------");
+
+        utenti = utenteRepo.findByNomeNullOrCognomeNull();
+        System.out.println(utenti);
+
+        System.out.println("--------------------------------");
+
+        utenti = utenteRepo.findByCreditoBetween(0, 10 * 100);
+        System.out.println(utenti);
+
+        System.out.println("--------------------------------");
+        System.out.println("The end");
+
+    }
 }
--- src/main/java/org/generation/jaita138/demo4/db/repository/UtenteRepo.java
+++ src/main/java/org/generation/jaita138/demo4/db/repository/UtenteRepo.java
@@ -1,10 +1,19 @@
 package org.generation.jaita138.demo4.db.repository;
 
+import java.util.List;
+
 import org.generation.jaita138.demo4.db.entity.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UtenteRepo extends JpaRepository<Utente, Long> {
+    
+    List<Utente> findByNomeStartsWith(String iniziale);
 
+    List<Utente> findByCreditoGreaterThan(int credito);
+
+    List<Utente> findByNomeNullOrCognomeNull();
+
+    List<Utente> findByCreditoBetween(int min, int max);
 }
